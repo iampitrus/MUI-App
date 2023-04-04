8c2fb56ebeebe66e0d4ed1e0a4cea13b9ca7beac
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2023-04-04 01:06:57 +0100

    Roboto font added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from "next/head";
 
 import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
 
@@ -18,17 +19,28 @@ function App() {
   });
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
-        <Navbar />
-        <Stack direction={"row"} spacing={2} justifyContent="space-between">
-          <Sidebar setMode={setMode} mode={mode} />
-          <Feed />
-          <Rightbar />
-        </Stack>
-        <Add />
-      </Box>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>My Mui Social Media App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@200;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ThemeProvider theme={darkTheme}>
+        <Box bgcolor={"background.default"} color={"text.primary"}>
+          <Navbar />
+          <Stack direction={"row"} spacing={2} justifyContent="space-between">
+            <Sidebar setMode={setMode} mode={mode} />
+            <Feed />
+            <Rightbar />
+          </Stack>
+          <Add />
+        </Box>
+      </ThemeProvider>
+    </>
   );
 }
 
